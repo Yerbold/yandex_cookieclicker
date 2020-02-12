1de8d872a5a6b58fe655833ee1acedfa43deeb1e
smail <smailov008@gmail.com>
2020-02-12 15:00:50 +0600

    Sprite for BigCookie image

---
+++ BigCookie.py
@@ -0,0 +1,26 @@
+import pygame
+from starter import all_sprites
+from starter import load_image
+
+
+class BigCookie(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.original_image = pygame.transform.scale(load_image('BigCookie.png', -1), (200, 200))
+        self.image = self.original_image
+        self.rect = self.image.get_rect()
+        self.rect.x = 450
+        self.rect.y = 40
+
+    def detect_click(self, mouse_pos):
+        if (self.rect.x < mouse_pos[0] < self.rect.x + 340
+                and self.rect.y < mouse_pos[1] < self.rect.y + 340):
+            return True
+
+    def change_pos(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
+    def transform_image(self, w):
+        self.image = pygame.transform.scale(self.original_image, (w, w))
+
--- starter.py
+++ starter.py
@@ -15,28 +15,6 @@ def load_image(name, colorkey=None):
     return image
 
 
-class BigCookie(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__(all_sprites)
-        self.original_image = pygame.transform.scale(load_image('BigCookie.png', -1), (200, 200))
-        self.image = self.original_image
-        self.rect = self.image.get_rect()
-        self.rect.x = 450
-        self.rect.y = 40
-
-    def detect_click(self, mouse_pos):
-        if (self.rect.x < mouse_pos[0] < self.rect.x + 340
-                and self.rect.y < mouse_pos[1] < self.rect.y + 340):
-            return True
-
-    def change_pos(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
-
-    def transform_image(self, w):
-        self.image = pygame.transform.scale(self.original_image, (w, w))
-
-
 pygame.init()
 screen = pygame.display.set_mode((1100, 750), pygame.FULLSCREEN, pygame.RESIZABLE)
 buildings = pygame.Surface((600, 400))
